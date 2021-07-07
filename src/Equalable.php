51e08b7f0c932fe26c52a187d9cd07f5179a4850
STEIN197 <nil20122013@gmail.com>
2021-07-07 17:17:18 +0500

    Initial commit

---
+++ src/Equalable.php
@@ -0,0 +1,16 @@
+<?php
+	namespace Stein197;
+
+	/**
+	 * This interface should be implemented by classes which instances are supposed to be compared/
+	 * @package Stein197
+	 */
+	interface Equalable {
+
+		/**
+		 * Compares the current object and another one.
+		 * @param Equalable $o Object to compare with.
+		 * @return bool `true` if objects are equal.
+		 */
+		public function equals(Equalable $o): bool;
+	}
